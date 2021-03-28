6955ead268f03ffb1c1be49807a4ad07f85e5536
Ram Mishra <rammishra@87-92-22-247.bb.dnainternet.fi>
2021-03-28 11:29:35 +0300

    mongoose schema and CRUD operations in routes

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const db = require('./db');
+const db = require("./db");
 
-db.on('connected', () => {
-  app.listen(3000, () => { console.log('express server started on port 3000')});
+app.use(express.urlencoded({ extended: false })); // for parsing html form x-www-form-urlencoded
+// and/or app.use(express.json()); // for parsing application/json
+app.use("/cat", require("./routes/catRoute"));
+
+db.on("connected", () => {
+  app.listen(3000, () => {
+    console.log("express server started on port 3000");
+  });
 });
-
